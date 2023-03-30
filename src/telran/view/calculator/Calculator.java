c9f30b1834e4511e3671fa4addf773ef3ade7ee4
maxastaf <max.a.staf@gmail.com>
2023-03-30 03:04:49 +0300

    CW #44

--- src/telran/view/calculator/Calculator.java
+++ src/telran/view/calculator/Calculator.java
@@ -5,47 +5,46 @@ import java.util.function.BiFunction;
 import telran.view.*;
 
 public class Calculator {
-	private static StandartInputOutput io = new StandartInputOutput();
 	
 	public static void main(String[] args) {
+		InputOutput io = new StandartInputOutput();
 		createMenu().perform(io);
-		io.writeLine("Thanks & Goodbye");
 	}
 
 	private static Menu createMenu() {
 		return new Menu("Calculator", 
 				new Menu("Arithmetic operations",
-						Item.of("Add numbers", el -> processNumbers("+", (a, b) -> a + b)),
-						Item.of("Subtract numbers", el -> processNumbers("-", (a, b) -> a - b)),
-						Item.of("Multiply numbers", el -> processNumbers("*", (a, b) -> a * b)),
-						Item.of("Divide numbers", el -> processNumbers("/", (a, b) -> a / b)),
+						Item.of("Add numbers", io -> processNumbers(io, "+", (a, b) -> a + b)),
+						Item.of("Subtract numbers", io -> processNumbers(io, "-", (a, b) -> a - b)),
+						Item.of("Multiply numbers", io -> processNumbers(io, "*", (a, b) -> a * b)),
+						Item.of("Divide numbers", io -> processNumbers(io, "/", (a, b) -> a / b)),
 						Item.exit()
 						),
 				new Menu("Date operations",
-						Item.of("Add days", el -> processDates("add", (a, b) -> a.plusDays(b))),
-						Item.of("Subtract days", el -> processDates("substract", (a, b) -> a.minusDays(b))),
+						Item.of("Add days", io -> processDates(io, "add", (a, b) -> a.plusDays(b))),
+						Item.of("Subtract days", io -> processDates(io, "substract", (a, b) -> a.minusDays(b))),
 						Item.exit()
 						),
 				Item.exit());
 	}
 	
-	private static void processDates(String operation, BiFunction<LocalDate, Integer, LocalDate> function) {
+	private static void processDates(InputOutput io, String operation, BiFunction<LocalDate, Integer, LocalDate> function) {
 		LocalDate date = io.readDateISO("Enter date", "Enter correct date");
 		int days = io.readInt(String.format("Enter days to %s", operation), "Enter correct number");
 		try {
-			io.writeLine(String.format("Date: %s, %s %s day(s), result: %s", date, operation, days, function.apply(date, days)));
+			io.writeLine(String.format("%s %s %s day(s) is: %s", date, operation, days, function.apply(date, days)));
 		} catch (Exception e) {
-			io.writeLine(String.format("Error while processing %s %s day(s) with date %s", operation, days, date));
+			io.writeLine(String.format("Error while processing %s %s day(s) with date %s: %s", operation, days, date, e.getMessage()));
 		}
 	}
 	
-	private static void processNumbers(String sign, BiFunction<Double, Double, Double> function) {
+	private static void processNumbers(InputOutput io, String sign, BiFunction<Double, Double, Double> function) {
 		double a = io.readNumber("Enter first number", "Enter correct double");
 		double b = io.readNumber("Enter second number", "Enter correct double");
 		try {
 			io.writeLine(String.format("%s %s %s = %s", a, sign, b, function.apply(a, b)));
 		} catch (Exception e) {
-			io.writeLine(String.format("Error processing  %s %s %s", a, sign, b));
+			io.writeLine(String.format("Error processing  %s %s %s: %s", a, sign, b, e.getMessage()));
 		}
 	}
 }
